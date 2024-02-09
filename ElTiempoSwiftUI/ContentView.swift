24909291f564fb5d7d72aa4d6320325feb475e37
di-saldana <dsl42@alu.ua.es>
2024-02-09 19:08:09 +0100

    first commit

---
+++ ElTiempoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ElTiempoSwiftUI
+//
+//  Created by Otto Colomina Pardo on 21/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let modelo = TiempoModelo()
+    var body: some View {
+        Text("Hola, soy SwiftUI")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ElTiempoSwiftUI/ElTiempoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElTiempoSwiftUIApp.swift
+//  ElTiempoSwiftUI
+//
+//  Created by Otto Colomina Pardo on 21/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ElTiempoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
