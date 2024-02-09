de549707d42e85e11292eb1b26d7e2e9bb539a9e
di-saldana <dsl42@alu.ua.es>
2024-02-09 19:43:32 +0100

    base

--- ElTiempoSwiftUI/ContentView.swift
+++ ElTiempoSwiftUI/ContentView.swift
@@ -9,8 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     let modelo = TiempoModelo()
+    let ciudades = ["Sanvi", "Londres", "Paris"]
+    
+    @State private var ciudad = 0
+    @State private var nombre = ""
+    @State private var suscrito = false
+    
     var body: some View {
-        Text("Hola, soy SwiftUI")
+        Form {
+            Section {
+                TextField("nombre:", text: $nombre)
+                Picker("Lugar", selection: $ciudad) {
+                    ForEach(0..<ciudades.count) { num in
+                        Text("\(self.ciudades[num])")
+                    }
+                }.pickerStyle(WheelPickerStyle())
+                Toggle("Suscribirse",isOn: $suscrito)
+            }
+            Section {
+                Text("Hola,\(nombre) de \(ciudades[ciudad])")
+                if suscrito {
+                   Text("¡Gracias por suscribirte!")
+                }
+                else {
+                   Text("¡Suscríbete, por favor!")
+                }
+            }
+//            Section {
+//                modelo.consultarTiempoActual(localidad: localidad) {
+//                    texto, direccionIcono in
+//                    //a partir de aquí, asignar "texto" y "direccionIcono" a propiedades @State
+//                    //para que se repinte la vista
+//                    //Ojo, tendrás que transformar direccionIcono String -> URL -> Data -> UIImage
+//                }
+//            }
+        }
     }
 }
 
@@ -19,3 +52,23 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+/*
+ 
+ En general, intenta que primero se vea el texto con el estado del tiempo y una vez esté funcionando ocúpate del icono con la imagen, recuerda que este último hay que cargarlo desde una url y la cosa se complica un poco con él.
+ Fíjate en la transparencia con el ejemplo de formulario
+ Puedes por ejemplo hacer dos secciones, una para el campo de texto donde se escribe la localidad y el botón y otra para mostrar los resultados en forma de texto y en forma gráfica
+ Los resultados solo los debes mostrar si no están vacíos, mira en la traspa cómo se hace para mostrar componentes de manera condicional
+ Para comunicarnos con el servidor podemos reutilizar el código del modelo de la versión sin SwiftUI. Para consultar el tiempo necesitas crear una instancia de TiempoModelo y sobre ella llamar al método consultarTiempoActual(localidad:callback:)
+
+ let modelo = TiempoModelo()
+ ...
+ modelo.consultarTiempoActual(localidad: localidad) {
+     texto, direccionIcono in
+     //a partir de aquí, asignar "texto" y "direccionIcono" a propiedades @State
+     //para que se repinte la vista
+     //Ojo, tendrás que transformar direccionIcono String -> URL -> Data -> UIImage
+ }
+ 
+ */
